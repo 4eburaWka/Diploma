263537e7000d04c88ae9a2fbf53fe9f64f4bb53c
4eburaWka <prikpasa14@gmail.com>
2025-05-07 22:10:46 +0300

    fix script

--- restart_app.sh
+++ restart_app.sh
@@ -3,10 +3,10 @@ git pull
 docker build -t glaucoma/be -f prediction/Dockerfile prediction/
 docker build -t glaucoma/nginx -f prediction/Dockerfile-nginx prediction/
 
-docker stop glaucoma_be
-docker rm glaucoma_be
-docker stop glaucoma_nginx
-docker rm glaucoma_nginx
+docker stop glaucoma_be || true
+docker rm glaucoma_be || true
+docker stop glaucoma_nginx || true
+docker rm glaucoma_nginx || true
 
 docker run -d --network core-network --name glaucoma_be glaucoma/be
 docker run -d --network core-network --name glaucoma_nginx glaucoma/nginx
